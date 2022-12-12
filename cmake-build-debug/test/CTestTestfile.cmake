2b8b9569a3b37cd6b2ec7bd90b795f90f10602b8
djrodriguez88 <daniel.rodriguez15@g.austincc.edu>
2022-12-11 18:13:29 -0600

    Question 2

---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/DJRod/CLionProjects/final-djrodriguez88/test
+# Build directory: C:/Users/DJRod/CLionProjects/final-djrodriguez88/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(craps_tests "C:/Users/DJRod/CLionProjects/final-djrodriguez88/cmake-build-debug/test/craps_tests.exe" "-s")
+set_tests_properties(craps_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/DJRod/CLionProjects/final-djrodriguez88/test/CMakeLists.txt;3;add_test;C:/Users/DJRod/CLionProjects/final-djrodriguez88/test/CMakeLists.txt;0;")
